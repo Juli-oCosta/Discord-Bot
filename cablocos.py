9491b24a097a1faaa14b0b5b76677b82ac4b4f87
Julio Cesar <145167974+Juli-oCosta@users.noreply.github.com>
2025-07-04 23:17:19 -0300

    Update cablocos.py

--- cablocos.py
+++ cablocos.py
@@ -16,8 +16,14 @@ dia_atual = datetime.date.today()
 ano, semana, dia_da_semana = dia_atual.isocalendar()
 
 # Caso o dia da semana for sex ta-feira, os cablocos avisam a gente por mensagem
+# Tem q fazer com que o código rode apenas uma vez toda sex ta obviamente
+# Tem q ser algo que fique ativo e guarde as informações, para contar quantas sex tas passaram para rodar a mensagem de aviso
 if dia_da_semana == 5:
     contagem_sex_ta = contagem_sex_ta + 1
-    print()
+    print(random.choice(mensagens_sex_ta))
 elif dia_da_semana == 4:
-    print()
+    print(random.choice(mensagens_quinta))
+
+if contagem_sex_ta == 3 and dia_da_semana == 3:
+    contagem_sex_ta = 0
+    print(random.choice(mensagens_aviso))
