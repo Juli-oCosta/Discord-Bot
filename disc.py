edd8949f80e03acf752074deb161c7feecf982cc
Julio Cesar <145167974+Juli-oCosta@users.noreply.github.com>
2025-07-07 21:38:19 -0300

    Update disc.py

--- disc.py
+++ disc.py
@@ -1,7 +1,12 @@
-import discord
+import os
+from dotenv import load_dotenv
+import discord 
 from discord import app_commands
 
-class MeuPrimeiroBot(discord.Client):
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+class Bot(discord.Client):
     def __init__(self):
         intents = discord.Intents.all()
         super().__init__(
@@ -9,28 +14,32 @@ class MeuPrimeiroBot(discord.Client):
             intents=intents
         )
         self.tree = app_commands.CommandTree(self)
-
+    
     async def setup_hook(self):
-        await self.tree.sync
-
+        await self.tree.sync()
+    
     async def on_ready(self):
-        print(f"O bot {self.user} foi ligado com sucesso.")
+        print(f"O Bot {self.user} entrou na conversa.")
+    
+bot = Bot()
 
-bot = MeuPrimeiroBot()
+@bot.tree.command(name="cumprimento_usuário", description="Faz uma saudação ao usuário que o chamar.")
+async def falaUsuario(interaction:discord.Interaction):
+    await interaction.response.send_message(f"Fala ae {interaction.user.mention}, ta tudo sussa?")
 
-@bot.tree.command(name="olá-usuário", description="Primeiro comando do bot.")
-async def olamundo(interaction:discord.Interaction):
-    await interaction.response.send_message(f"Olá {interaction.user.mention}!")
-
-@bot.tree.command(name="soma", description="Some dois números distintos.")
+@bot.tree.command(name="soma", description="Some dois números.")
 @app_commands.describe(
     numero1 = "Primeiro número a somar.",
-    numero2 = "Segundo númeroa somar."
+    numero2 = "Segundo número a somar." 
 )
-async def somaNumero(interaction:discord.Interaction, numero1:int, numero2:int):
-    numero_somado = numero1 + numero2
-    await interaction.response.send_message(f"O número somado é: {numero_somado}", ephemeral=True)
-
+async def somaNumeros(interaction:discord.Interaction, numero1:int, numero2:int):
+    resultado = numero1 + numero2
+    mensagem = f"A opa {interaction.user.mention} o negócio é o seguinte, falaro que {numero1} + {numero2} = {resultado}"
+    if resultado == 24:
+        mensagem += " (viisheee)"
+        
+    await interaction.response.send_message(mensagem, ephemeral=True)
+    
 # For safety reasons, the token should not be shared.
 # Por questões de segurança, o token não deve ser compartilhado
-bot.run("") # token
+bot.run(TOKEN)
